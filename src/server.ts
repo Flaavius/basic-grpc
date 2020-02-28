4a40a03bd3dfc68e351da222a3dded7d79ab721c
Flaavius <flaav12@gmail.com>
2020-02-29 00:57:29 +0200

    updated RPCError class

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import { SSLCred, protoLoaderOptions } from ".";
 export class RPCError extends Error {
   public readonly code: number;
   public readonly details: string;
-  constructor(code: number, details: string) {
+  constructor({ code, details }: { code: number, details: string }) {
     super(details);
     this.code = code;
     this.details = details;
